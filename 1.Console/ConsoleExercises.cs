7b6e45006c3043a2113374f8551ae90feffd6263
FedericoPrieto <jonathan-lugos@hotmail.com>
2019-05-09 22:00:51 -0300

    Clase 3

--- 1.Console/ConsoleExercises.cs
+++ 1.Console/ConsoleExercises.cs
@@ -3,9 +3,23 @@ using System;
 namespace CSharpExercises{
     public class ConsoleExercises {
         public void AskAge(){
+
             Console.WriteLine("¿Cuántos años tenes?: ");
             string age = Console.ReadLine();
             Console.WriteLine("Wow, no pareces de " + age + " años");
+
+ //Pedir nombre, apellido y documento e imprimir toda la información al final.
+            Console.WriteLine("Escribe tu nombre");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu apellido");
+            string apellido = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu DNI");
+            string dni = Console.ReadLine();
+
+            Console.WriteLine("Tu nombre es {0}, tu apellido es {1} y tu DNI es {2}", nombre, apellido, dni);
+
             Console.ReadLine();
         }
     }
--- 3.TiposDatos/DataTypeExercises.cs
+++ 3.TiposDatos/DataTypeExercises.cs
@@ -4,12 +4,39 @@ namespace CSharpExercises{
     public class DataTypeExercises{
         public void ValueSum(){
             Console.WriteLine("Ingrese un valor a sumar:");
-            var firstValue = Console.ReadLine();
+            var firstValue =  Console.ReadLine();
 
             Console.WriteLine("Ingrese otro valor a sumar:");
             var secondValue = Console.ReadLine();
 
-            Console.WriteLine("El resultado de la suma es: {0}", firstValue + secondValue);
+//con control de errores 
+//            int first = Int32.Parse(firstValue);
+//            int second = Int32.Parse(secondValue);
+
+            int first = 0;
+            int second =0;
+            bool couldParseFirst = Int32.TryParse(firstValue, out first);
+
+            if (couldParseFirst) {
+
+                bool couldParseSecond = Int32.TryParse(secondValue, out second);
+
+                if (couldParseSecond) {
+
+                    Console.WriteLine("El resultado de la suma es: {0}", first + second);
+
+                }
+                else
+                {
+                    Console.WriteLine("El segundo valor no es numérico");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("El primer valor no es numérico");
+            }
+
         }
     }
 }
