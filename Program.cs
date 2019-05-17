340e51cd74fafe5207029e67a34e726bcc13c7bd
FedericoPrieto <federigogprieto@gmail.com>
2019-05-17 09:18:58 -0300

    arreglo

--- Program.cs
+++ Program.cs
@@ -6,20 +6,34 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            //1.
-            //ConsoleExercises console = new ConsoleExercises();
-            //console.AskAge();
+            
+            System.Console.WriteLine("Escriba el número de ejercicio:");
+            string x = System.Console.ReadLine();
 
+            switch(x){
+            case "1":
+                    //1.
+                    ConsoleExercises console = new ConsoleExercises();
+                    console.AskAge();
+                    break;
             //Pedir nombre, apellido y documento e imprimir toda la información al final.
 
-            //2.
-            //VariableExercises variable = new VariableExercises();
-            //variable.Concatenador();
-            //
+            case "2":
+                    //2.
+                    VariableExercises variable = new VariableExercises();
+                    variable.Concatenador();
+                    break;
             
-            //3.
-            DataTypeExercises dataType = new DataTypeExercises();
-            dataType.ValueSum();
+            case "3":
+                    //3.
+                    DataTypeExercises dataType = new DataTypeExercises();
+                    dataType.ValueSum();
+                    break;
+            default:
+                    System.Console.WriteLine("No existe ejercicio");
+                    break;
+
+            }
         }
     }
 }
