972ff609628a52c0d1761b0ab7adc2ad4500dd55
FedericoPrieto <federicogprieto@gmail.com>
2019-05-21 22:03:53 -0300

    21052019

---
+++ EjerciciosVarios/Cadenas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                // Escribir un programa que permita ingresar el nombre completo de una persona e Informe 
+            //cuantos caracteres tiene este nombre.
+                // Luego solicite el ingreso de una cadena de caracteres e informe si el nombre incluye 
+            //o no esta cadena de caracteres.
+                // Finalmente, que imprima el nombre al revés.
+            Console.WriteLine("Escriba Nombre y Apellido");
+            string nombre;
+            nombre=Console.ReadLine();
+
+            Console.WriteLine(nombre.Length);
+
+            Console.WriteLine("Escriba Otro Nombre y Apellido");
+            string nombre1;
+            nombre1=Console.ReadLine();
+
+            if (Console.WriteLine(nombre == nombre1))
+            {
+
+                Console.WriteLine("Los nombres son iguales");
+
+            }
+            Else {
+
+                Console.WriteLine("Los nombres son distintos");
+
+            }
+
+        }
+    }
+}
---
+++ EjerciciosVarios/Pruebas/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Pruebas
+
+   {
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimeArrayInvertido();
+            CreaArray();
+
+        }
+        private static void ImprimeArrayInvertido()
+        {
+            int[] list = { 21, 73, 14, 24, 20, 3, 1 };
+            int[] temp = new int[list.Length];
+ 
+
+            Array.Copy(list, temp, list.Length);
+ 
+
+            Console.WriteLine("Array Original: ");
+            Utilidades.Show(list);
+ 
+            Utilidades.Show(list);
+
+            Array.Reverse(temp);
+            Console.WriteLine("Array Invertido: ");
+            Utilidades.Show(temp);
+ 
+            Utilidades.Show(temp);
+
+            Array.Sort(list);
+            Console.WriteLine("Arreglo Ordenado: ");
+            Utilidades.Show(list);
+
+            Utilidades.Show(list);
+        }
+
+        //static void myArray()
+        static void CreaArray()
+        {
+          
+                int[] myArray = { 21, 73, 14, 24, 20, 3, 1 };
+          
+            Console.Write("[ ");
+            for(int i=0; i < myArray.Length; i++)
+
+            Console.WriteLine("Ingrese cant. de elementos");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[cantidad];
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if(i > 0)
+                for (int j = i + 1; j < vector.Length; j++)
+                {
+                    Console.Write(",");
+                    if (vector[i] > vector[j])
+                    {
+                        int aux = vector[i];
+                        vector[i] = vector[j];
+                        vector[j] = aux;
+                    }
+                }
+                Console.Write(myArray[i]);
+            }
+            Console.WriteLine("]");
+
+            Console.WriteLine("Vector Ordenado: ");
+            Utilidades.Show(vector);
+            Console.ReadKey();
+
+        }
+
+    }
+    }
